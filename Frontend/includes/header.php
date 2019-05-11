6b90536691a3e5351fb5d6e83df1829b8ff1b336
loilv <loilv@vaymuon.vn>
2019-05-11 12:21:25 +0700

    upload project

---
+++ Frontend/includes/header.php
@@ -0,0 +1,108 @@
+<header>
+    <div class="wrap-header">
+        <div class="container-fluid">
+            <div class="header-content">
+                <div class="header-logo">
+                    <img src="/images/header/logo.png" alt="">
+                </div>
+                <div class="header-navs">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-flag nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="nav-flag"><img src="/images/header/vietnam.png" alt=""></i>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-nav-nations" aria-labelledby="navbarDropdown">
+                                        <div class="wrap-nav-nations">
+                                            <ul class="nav-nations-content">
+                                                <li class="nav-nations">
+                                                    <a href="">
+                                                        <div class="nation-flag">
+                                                            <img src="/images/header/vietnam.png" alt="">
+                                                        </div>
+                                                        <p class="nation-name">Thailand</p>
+                                                    </a>
+                                                </li>
+                                                <li class="nav-nations">
+                                                    <a href="">
+                                                        <div class="nation-flag">
+                                                            <img src="/images/header/vietnam.png" alt="">
+                                                        </div>
+                                                        <p class="nation-name">Vietnamese</p>
+                                                    </a>
+                                                </li>
+                                                <li class="nav-nations">
+                                                    <a href="">
+                                                        <div class="nation-flag">
+                                                            <img src="/images/header/vietnam.png" alt="">
+                                                        </div>
+                                                        <p class="nation-name">Vietnamese</p>
+                                                    </a>
+                                                </li>
+                                                <li class="nav-nations">
+                                                    <a href="">
+                                                        <div class="nation-flag">
+                                                            <img src="/images/header/vietnam.png" alt="">
+                                                        </div>
+                                                        <p class="nation-name">Vietnamese</p>
+                                                    </a>
+                                                </li>
+                                                <li class="nav-nations">
+                                                    <a href="">
+                                                        <div class="nation-flag">
+                                                            <img src="/images/header/vietnam.png" alt="">
+                                                        </div>
+                                                        <p class="nation-name">Vietnamese</p>
+                                                    </a>
+                                                </li>
+                                                <li class="nav-nations">
+                                                    <a href="">
+                                                        <div class="nation-flag">
+                                                            <img src="/images/header/vietnam.png" alt="">
+                                                        </div>
+                                                        <p class="nation-name">Vietnamese</p>
+                                                    </a>
+                                                </li>
+
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Borrower</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Investor</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Strategic investment</a>
+                                </li>
+                                <li class="nav-item dropdown dropdown-investor">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        become a strategic investor
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item nav-language">
+                                    <a class="nav-link disabled" href="#">Language: ENG</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ Frontend/index.php
@@ -0,0 +1,321 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lendmo asia</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./plugin/owlcarousel/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="./plugin/owlcarousel/assets/owl.theme.default.min.css">
+</head>
+<body>
+<!--    Include Header      -->
+    <?php include('./includes/header.php')?>
+<!--    Section Banner Top-->
+    <section class="banner-top">
+        <div class="bannser-top-img">
+            <img src="./images/header/banner.png" alt="">
+        </div>
+    </section>
+<!--    Section Block operation-->
+    <section class="block-operation">
+        <div class="block-operation-title">
+            <h3>Operation model</h3>
+            <p class="operation-description">Lendmo is not a bank, not a financial company <br>
+                Lendmo uses technology to connect the needs of borrowers and investors by providing give borrowers the <br>
+                best price, the highest profit investor.</p>
+        </div>
+        <div class="block-operation-wrap-content">
+            <div class="operation-content">
+                <div class="operation-content-img">
+                    <img src="./images/block_operation/operation_images_01.png" alt="">
+                </div>
+                <div class="operation-description">The borrower goes to Lendmo and sends a loan request</div>
+                <div class="operation-arrow"></div>
+            </div>
+            <div class="operation-content">
+                <div class="operation-content-img">
+                    <img src="./images/block_operation/operation_images_02.png" alt="">
+                </div>
+                <div class="operation-description">Lendmo reviews the borrower's information and sends it to investors</div>
+                <div class="operation-arrow"></div>
+            </div>
+            <div class="operation-content">
+                <div class="operation-content-img">
+                    <img src="./images/block_operation/operation_images_03.png" alt="">
+                </div>
+                <div class="operation-description">Investors approve and decide lending</div>
+            </div>
+        </div>
+    </section>
+<!--    Section feature -->
+    <section class="feature-lendmo">
+        <div class="feature-lendmo-title">Special features of Lendmo</div>
+        <div class="feature-content">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="feature-content-left">
+                        <div class="image-circle"><img src="./images/block_operation/circle.png" alt=""></div>
+                        <div class="image-phone">
+                            <div class="iphone-x"><img src="./images/block_operation/iPhone-X.png" alt=""></div>
+                            <div class="boarding"><img src="./images/block_operation/Onboarding–2.png" alt=""></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="feature-content-right">
+                        <ul>
+                            <li class="instuctions-steps">
+                                <div class="step-content">
+                                    <div class="circle-number step-active"><span>1</span></div>
+                                    <div class="step-description">
+                                        <h3>Đăng nhập trong “chớp mắt” chỉ cần số điện thoại</h3>
+                                        <p>Việc vay tiền trở nên đơn giản, tiện dụng ngay trên “dế yêu” của bạn</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="instuctions-steps">
+                                <div class="step-content">
+                                    <div class="circle-number"><span>2</span></div>
+                                    <div class="step-description">
+                                        <h3>“Một click” nhanh chóng tạo yêu cầu vay</h3>
+                                        <p>Giao diện xanh thân thiện, Vay Mượn giúp bạn hoàn thiện hồ sơ chỉ với ảnh chụp, 100% không thu giữ giấy tờ, không thế chấp tài sản</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="instuctions-steps">
+                                <div class="step-content">
+                                    <div class="circle-number"><span>3</span></div>
+                                    <div class="step-description">
+                                        <h3>Vay tín chấp hạn mức “khủng” lên tới 10 triệu đồng</h3>
+                                        <p>Bạn thậm chí có thể vay lại nhiều lần nếu có điểm tín dụng tốt</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="instuctions-steps">
+                                <div class="step-content">
+                                    <div class="circle-number"><span>4</span></div>
+                                    <div class="step-description">
+                                        <h3>Lãi suất và phí dịch vụ được tính và hiển thị ngay trên màn hình</h3>
+                                        <p>Vay Mượn sẽ giúp bạn tính số tiền phải thanh toán khi đến hạn một cách minh bạch, rõ ràng</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="instuctions-steps">
+                                <div class="step-content">
+                                    <div class="circle-number"><span>5</span></div>
+                                    <div class="step-description">
+                                        <h3>Gửi yêu cầu vay - 30 phút có tiền ngay</h3>
+                                        <p>Thời gian chờ giải ngân "thần tốc" khiến nhiều khách hàng tin yêu</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="instuctions-steps">
+                                <div class="step-content">
+                                    <div class="circle-number"><span>6</span></div>
+                                    <div class="step-description">
+                                        <h3>Kiếm tiền cùng Vay Mượn</h3>
+                                        <p>Bạn có thể gia tăng mức thu nhập hàng tháng của mình 2-10 triệu, thậm chí không giới hạn khi chia sẻ mã giới thiệu của mình tới những người xung quanh</p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <article class="block-steps">
+                        <div class="block-step-content">
+                            <h3 class="step-content-title feature-lendmo-title">4-step loan and refund super speed</h3>
+                            <div class="row step-wrapper">
+                                <div class="col-md-3 step-wrap-content">
+                                    <div class="title-image"><img src="./images/block_operation/step_image_01.png" alt=""></div>
+                                    <div class="circle-number"><span>1</span></div>
+                                    <div class="step-title-text">
+                                        <h3>Registration</h3>
+                                        <p class="step-title-des">
+                                            Download the Lendmo app Register online
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 step-wrap-content">
+                                    <div class="title-image"><img src="./images/block_operation/step_image_01.png" alt=""></div>
+                                    <div class="circle-number"><span>1</span></div>
+                                    <div class="step-title-text">
+                                        <h3>Registration</h3>
+                                        <p class="step-title-des">
+                                            Download the Lendmo app Register online
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 step-wrap-content">
+                                    <div class="title-image"><img src="./images/block_operation/step_image_01.png" alt=""></div>
+                                    <div class="circle-number"><span>1</span></div>
+                                    <div class="step-title-text">
+                                        <h3>Registration</h3>
+                                        <p class="step-title-des">
+                                            Download the Lendmo app Register online
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 step-wrap-content">
+                                    <div class="title-image"><img src="./images/block_operation/step_image_01.png" alt=""></div>
+                                    <div class="circle-number"><span>1</span></div>
+                                    <div class="step-title-text">
+                                        <h3>Registration</h3>
+                                        <p class="step-title-des">
+                                            Download the Lendmo app Register online
+                                        </p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </article>
+                    <div class="image-upper"><img src="./images/block_operation/layer_background.png" alt=""></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="block-bottom">
+        <article class="group-partner">
+            <h3 class="step-content-title feature-lendmo-title title-partner">Our partner</h3>
+            <div class="row wrap-logo">
+                <div class="col-md-2 logo-partner">
+                    <img src="./images/block_operation/logo_viettel.png" alt="">
+                </div>
+                <div class="col-md-2 logo-partner">
+                    <img src="./images/block_operation/logo-tiki.png" alt="">
+                </div>
+                <div class="col-md-2 logo-partner">
+                    <img src="./images/block_operation/logo_mobifione.png" alt="">
+                </div>
+                <div class="col-md-2 logo-partner">
+                    <img src="./images/block_operation/logo_techcombank.png" alt="">
+                </div>
+                <div class="col-md-2 logo-partner">
+                    <img src="./images/block_operation/logo_vtv.png" alt="">
+                </div>
+            </div>
+        </article>
+        <article class="blog-posts">
+            <div class="line-top"></div>
+            <h3 class="step-content-title feature-lendmo-title title-partner">Blog posts</h3>
+            <div class="blog-post-content">
+                <div class="owl-carousel owl-theme">
+                    <div class="item">
+                        <div class="card" style="width: 21rem;">
+                            <img class="card-img-top" src="./images/block_operation/blog_hue.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Huế lung linh, mơ mộng qua 12 bức ảnh đẹp nhất 2018</h5>
+                                <p class="date-public">2019-04-23, 14:30pm</p>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="readmore-post">View more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="card" style="width: 21rem;">
+                            <img class="card-img-top" src="./images/block_operation/blog_hue.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Huế lung linh, mơ mộng qua 12 bức ảnh đẹp nhất 2018</h5>
+                                <p class="date-public">2019-04-23, 14:30pm</p>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="readmore-post">View more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="card" style="width: 21rem;">
+                            <img class="card-img-top" src="./images/block_operation/blog_hue.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Huế lung linh, mơ mộng qua 12 bức ảnh đẹp nhất 2018</h5>
+                                <p class="date-public">2019-04-23, 14:30pm</p>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="readmore-post">View more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="card" style="width: 21rem;">
+                            <img class="card-img-top" src="./images/block_operation/blog_hue.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Huế lung linh, mơ mộng qua 12 bức ảnh đẹp nhất 2018</h5>
+                                <p class="date-public">2019-04-23, 14:30pm</p>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="readmore-post">View more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="card" style="width: 21rem;">
+                            <img class="card-img-top" src="./images/block_operation/blog_hue.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Huế lung linh, mơ mộng qua 12 bức ảnh đẹp nhất 2018</h5>
+                                <p class="date-public">2019-04-23, 14:30pm</p>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="readmore-post">View more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="block-prize">
+            <h3 class="step-content-title feature-lendmo-title title-partner">Prize</h3>
+            <div class="wrap-prize">
+                <ul class="prize-content">
+                    <li class="prize-item"><img src="./images/block_operation/giai_thuong.png" alt=""></li>
+                    <li class="prize-item"><img src="./images/block_operation/giai_thuong.png" alt=""></li>
+                    <li class="prize-item"><img src="./images/block_operation/giai_thuong.png" alt=""></li>
+                </ul>
+            </div>
+        </article>
+        <article class="block-video">
+            <h3 class="step-content-title feature-lendmo-title title-partner">Video about us</h3>
+            <div class="wrap-video">
+                <div class="video-content">
+                    <div class="main-left">
+                        <img src="./images/block_operation/vtv1.jpg" alt="">
+                    </div>
+                    <div class="main-right">
+                        <img class="video-top" src="./images/block_operation/vtc2.png" alt="">
+                        <img class="video-top" src="./images/block_operation/vtc2.png" alt="">
+                        <img src="./images/block_operation/vtc2.png" alt="">
+                        <img src="./images/block_operation/vtc2.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </article>
+    </section>
+    <section class="contact">
+        <div class="contact-title">
+            <h3>Contact Us</h3>
+        </div>
+        <div class="contact-detail">
+            <div class="contact-phone">
+                <div class="icon-contact"></div>
+                <div class="text-contact">
+                    <h3>Phone</h3>
+                    <p>+(84) 123456789</p>
+                    <p>+(84) 123456789</p>
+                </div>
+            </div>
+            <div class="contact-email">
+                <div class="icon-contact"></div>
+                <div class="text-contact">
+                    <h3>Phone</h3>
+                    <p>+(84) 123456789</p>
+                    <p>+(84) 123456789</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+<script src="./js/jquery-3.4.1.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+<script src="./js/script.js"></script>
+<script src="./plugin/owlcarousel/owl.carousel.min.js"></script>
+</html>
